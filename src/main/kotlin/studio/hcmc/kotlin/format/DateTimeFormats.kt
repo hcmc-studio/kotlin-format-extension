1908a35f3079b483a217b2fcd6b147767be0cc1b
Ji-Hwan KIM <nodealdev@gmail.com>
2023-09-20 23:01:17 +0900

    [Script push]

--- src/main/kotlin/studio/hcmc/kotlin/format/DateTimeFormats.kt
+++ src/main/kotlin/studio/hcmc/kotlin/format/DateTimeFormats.kt
@@ -1,17 +1,24 @@
 package studio.hcmc.kotlin.format
 
-import kotlinx.datetime.Instant
-import kotlinx.datetime.LocalTime
-import kotlinx.datetime.toKotlinInstant
+import kotlinx.datetime.*
 
 fun java.sql.Date.toKotlinInstant(): Instant {
-    return toInstant().toKotlinInstant()
+    return LocalDateTime(toLocalDate().toKotlinLocalDate(), LocalTime.zero).toInstant(TimeZone.UTC)
 }
 
 fun java.util.Date.toKotlinInstant(): Instant {
     return toInstant().toKotlinInstant()
 }
 
-private val _LocalTimeZero = LocalTime(0, 0, 0, 0)
+private val LocalTimeZero = LocalTime(0, 0, 0, 0)
 
-val LocalTime.Companion.zero: LocalTime get() = _LocalTimeZero
+val LocalTime.Companion.zero: LocalTime get() = LocalTimeZero
+
+fun main() {
+    val date = java.sql.Date.valueOf("1900-01-01")
+    println(date.time)
+    println(date.toLocalDate())
+    println(date.toLocalDate().toKotlinLocalDate())
+    println(LocalDateTime(date.toLocalDate().toKotlinLocalDate(), LocalTimeZero))
+    println(LocalDateTime(date.toLocalDate().toKotlinLocalDate(), LocalTimeZero).toInstant(TimeZone.UTC))
+}
